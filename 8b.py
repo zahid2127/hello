13da85f2e238c91797e649af6702b2327043f1e0
zahid2127 <jamalzahid777@gmail.com>
2022-08-04 10:36:56 +0530

    huzu

--- 8b.py
+++ 8b.py
@@ -1,24 +1,29 @@
-open_list = ['[', '{', '(']
-close_list = [']', '}', ')']
-def checkParanthesis(myStr):
-    stack = []
-    for i in myStr:
-        if i in open_list:
-            stack.append(i)
-        elif i in close_list:
-            pos = close_list.index(i)
-            if ((len(stack) > 0) and (open_list[pos] == stack[len(stack) - 1])
-                stack.pop()
-            else:
-                return 'Unbalanced'
-    if len(stack) == 0:
-        return 'Balanced'
-    else:
-        return 'Unbalanced'
+open_list = ["[","{","("]
+close_list = ["]","}",")"]
+def checkparentheses(myStr):
+	stack = []
+	for i in myStr:
+		if i in open_list:
+			stack.append(i)
+		elif i in close_list:
+			pos = close_list.index(i)
+			if ((len(stack) > 0) and
+				(open_list[pos] == stack[len(stack)-1])):
+				stack.pop()
+			else:
+				return "Unbalanced"
+	if len(stack) == 0:
+		return "Balanced"
+	else:
+		return "Unbalanced"
 
-string = '{[]{()}}'
-print(string, '-', checkParanthesis(string))
-string = '[{}{})(]'
-print(string, '-', checkParanthesis(string))
-string = '((()))'
-print(string, '-', checkParanthesis(string))
+
+# Driver code
+string = "{[]{()}}"
+print(string,"-", checkparentheses(string))
+
+string = "[{}{})(]"
+print(string,"-", checkparentheses(string))
+
+string = "((()"
+print(string,"-",checkparentheses(string))
--- Binary Search Tree.py
+++ Binary Search Tree.py
@@ -1,59 +1,72 @@
 class Node:
-    def __del__(self,key):
+    def __init__(self, key):
         self.key = key
         self.left = None
         self.right = None
 def inorder(root):
     if root is not None:
+        # Traverse left
         inorder(root.left)
-        print(str(root.key)+"->",end='')
+        print(str(root.key) + "->", end=' ')
         inorder(root.right)
-def insert(node,key):
+def insert(node, key):
     if node is None:
         return Node(key)
     if key < node.key:
-        node.left = insert(node.left,key)
+        node.left = insert(node.left, key)
     else:
-        node.right = insert(node.right,key)
+        node.right = insert(node.right, key)
+
     return node
 def minValueNode(node):
     current = node
+
+    # Find the leftmost leaf
     while(current.left is not None):
         current = current.left
+
     return current
-def deleteNode(root,key):
+def deleteNode(root, key):
+
+    # Return if the tree is empty
     if root is None:
         return root
     if key < root.key:
-        root.left = deleteNode(root.left,key)
+        root.left = deleteNode(root.left, key)
     elif(key > root.key):
-        root.right = deleteNode(root.right,key)
+        root.right = deleteNode(root.right, key)
     else:
         if root.left is None:
             temp = root.right
             root = None
             return temp
+
         elif root.right is None:
             temp = root.left
             root = None
             return temp
         temp = minValueNode(root.right)
+
         root.key = temp.key
-        root.right = deleteNode(root.right,temp.key)
+        root.right = deleteNode(root.right, temp.key)
+
     return root
 
+
 root = None
-root = insert(root,8)
-root = insert(root,3)
-root = insert(root,1)
-root = insert(root,6)
-root = insert(root,7)
-root = insert(root,10)
-root = insert(root,14)
-root = insert(root,4)
-print("Inorder traversal:",end='')
+root = insert(root, 8)
+root = insert(root, 3)
+root = insert(root, 1)
+root = insert(root, 6)
+root = insert(root, 7)
+root = insert(root, 10)
+root = insert(root, 14)
+root = insert(root, 4)
+
+print("Inorder traversal: ", end=' ')
 inorder(root)
+
 print("\nDelete 10")
-root = deleteNode(root,10)
-print("Inorder traversal:",end='')
+root = deleteNode(root, 10)
+print("Inorder traversal: ", end=' ')
 inorder(root)
